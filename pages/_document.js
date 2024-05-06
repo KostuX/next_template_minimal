4cf0b0932fe9345741bc18b70786d84927061dfa
KostuX <goldiskiker@gmail.com>
2024-05-06 16:07:49 +0100

    cleanup

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,4 @@
-import { Html,Head, Main, NextScript } from "next/document";
-
-
+import { Html, Head, Main, NextScript } from "next/document";
 
 export const viewport = {
   themeColor: [
@@ -11,20 +9,12 @@ export const viewport = {
 
 export default function Document() {
   return (
-    
-      <Html lang="en">
-       <Head/>
-
-        <body className="h-screen bg-background ">
-        
-     
-            <Main />
-          
-            <NextScript />
-        </body>
-
-       
-      </Html>
-   
+    <Html lang="en">
+      <Head />
+      <body className="h-screen bg-background ">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
   );
 }
