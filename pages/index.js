bbdd689a2b36ae99bc85c579cdefc7074dc8edff
Kostux <goldiskiker@gmail.com>
2024-05-07 11:22:22 +0100

    work on color

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <DefaultLayout>
       <div className="justify-between h-full text-center ">
-        <div className="border  m-5">Top Center</div>
+        <div className="border  m-5 text-black dark:text-white">Top Center</div>
 
         <span className=" grid grid-cols-2 gap-4  m-3 h-56 ">
-          <span className="border">Left Panel</span>
+          <span className="border ">Left Panel</span>
           <span className="border">Right Panel</span>
         </span>
       </div>
